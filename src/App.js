69eb88379431150b9db2d5485d074dbfbc64e656
Gokhan Yesilova <gokhanyesil1661@gmail.com>
2023-07-07 14:37:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/header/Header';
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import Main from './components/main/Main';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Navbar/>
+       <Header/>
+       <Main />
+       
+      
+    </div>
+  );
+}
+
+export default App;
